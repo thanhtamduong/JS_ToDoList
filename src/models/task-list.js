41ab0e7e2576b887b7065044339a54dea642f9b7
Duong Thanh Tam <thanhtamduong555@gmail.com>
2023-02-11 20:05:46 +0700

    push controller and models

--- src/models/task-list.js
+++
@@ -1,3 +0,0 @@
-function TaskList() {
-  var arr = [];
-}
--- src/models/task.js
+++
@@ -1,5 +0,0 @@
-function Task(_idTask, _taskName, _status) {
-  this.id = _idTask;
-  this.taskName = _taskName;
-  this.status = _status;
-}
